99350ee0f5579696242f69bab41cd7b03085d549
Chad Johnston <cjohnston@megatome.com>
2015-09-08 14:39:44 -0600

    Initial changes

--- IPAddress/AppDelegate.swift
+++ IPAddress/AppDelegate.swift
@@ -13,13 +13,53 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
 
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
+        let menu = NSMenu()
+
+        menu.addItem(NSMenuItem(title: "Quit", action: Selector("terminate:"), keyEquivalent: "q"))
+
+        statusItem.menu = menu
+        statusItem.title = getIFAddresses()[0]
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
+    }
+    
+    func getIFAddresses() -> [String] {
+        var addresses = [String]()
+        
+        // Get list of all interfaces on the local machine:
+        var ifaddr : UnsafeMutablePointer<ifaddrs> = nil
+        if getifaddrs(&ifaddr) == 0 {
+            
+            // For each interface ...
+            for (var ptr = ifaddr; ptr != nil; ptr = ptr.memory.ifa_next) {
+                let flags = Int32(ptr.memory.ifa_flags)
+                var addr = ptr.memory.ifa_addr.memory
+                
+                // Check for running IPv4, IPv6 interfaces. Skip the loopback interface.
+                if (flags & (IFF_UP|IFF_RUNNING|IFF_LOOPBACK)) == (IFF_UP|IFF_RUNNING) {
+                    if addr.sa_family == UInt8(AF_INET) || addr.sa_family == UInt8(AF_INET6) {
+                        
+                        // Convert interface address to a human readable string:
+                        var hostname = [CChar](count: Int(NI_MAXHOST), repeatedValue: 0)
+                        if (getnameinfo(&addr, socklen_t(addr.sa_len), &hostname, socklen_t(hostname.count),
+                            nil, socklen_t(0), NI_NUMERICHOST) == 0) {
+                                if let address = String.fromCString(hostname) {
+                                    addresses.append(address)
+                                }
+                        }
+                    }
+                }
+            }
+            freeifaddrs(ifaddr)
+        }
+        
+        return addresses
     }
 
 
---
+++ IPAddress/IPAddress-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  IPAddress-Bridging-Header.h
+//  IPAddress
+//
+//  Created by Chad Johnston on 9/8/15.
+//  Copyright (c) 2015 Megatome Technologies LLC. All rights reserved.
+//
+
+#ifndef IPAddress_IPAddress_Bridging_Header_h
+#define IPAddress_IPAddress_Bridging_Header_h
+
+#include <ifaddrs.h>
+
+#endif
