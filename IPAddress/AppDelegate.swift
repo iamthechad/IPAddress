284f5e42cde991de6ce191aff088e77436ad4be6
Chad Johnston <cjohnston@megatome.com>
2015-09-08 13:24:51 -0600

    Initial Commit

---
+++ IPAddress/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  IPAddress
+//
+//  Created by Chad Johnston on 9/8/15.
+//  Copyright (c) 2015 Megatome Technologies LLC. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
